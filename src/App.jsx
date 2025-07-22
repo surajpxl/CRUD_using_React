41f3035bc383047ae657b171eaf6aaa8a95bb098
surajpxl <guptasuraj4455@gmail.com>
2025-07-23 00:53:35 +0530

    manually data shown done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { employeeData } from './EmployeeData'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, SetData] = useState([]);
+
+  useEffect(() => {
+    SetData(employeeData)
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+   <div className='App'>
+    <table className='table-auto w-full text-center border border-gray-300'>
+      <thead>
+        <tr className='hover:bg-gray-300'>
+          <td>Sr.No</td>
+          <td>Id</td>
+          <td>First Name</td>
+          <td>Last Name</td>
+          <td>age</td>
+          <td>Actions</td>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.map((item, index) => {
+            return (
+              <tr key={index} className='hover:bg-gray-200'>
+                <td>{index+ 1}</td>
+                <td>{item.id}</td>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td>{item.age}</td>
+                <td>
+                  <button className='bg-blue-500 rounded-sm m-2 px-2'>Edit</button>
+                  <button className='bg-red-700 rounded-sm m-2 px-2
+                  '>Delete</button>
+                </td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+   </div>
+   </>
   )
 }
 
---
+++ src/EmployeeData.jsx
@@ -0,0 +1,26 @@
+export const employeeData = [
+    {
+        id: 1,
+        firstName : "John",
+        lastName: "Doe",
+        age: 30,
+    },
+    {
+        id: 2,
+        firstName : "abhi",
+        lastName: "kumar",
+        age: 25,
+    },
+    {
+        id: 3,
+        firstName : "suresh",
+        lastName: "agrawal",
+        age: 28,
+    },
+    {
+        id: 4,
+        firstName : "george",
+        lastName: "smith",
+        age: 35,
+    }
+]
